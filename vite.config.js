de684f0b22fd67f67fa488cfc63054ddd79c46e5
sowmya198 <38074750+sowmya198@users.noreply.github.com>
2024-07-30 13:55:30 +0200

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/sowmya-AS/',
+  build: {
+    outDir: 'dist',
+  },
   plugins: [react()],
   css: {
     modules: {
